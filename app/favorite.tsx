5f94a645059e17623c5d2ce6870a63e632646c75
Mayopi <cyade007@gmail.com>
2024-06-01 17:27:56 +0700

    feat: favorite screen

---
+++ app/favorite.tsx
@@ -0,0 +1,46 @@
+import { Text, StyleSheet, View, FlatList } from "react-native";
+import useFavoriteStore from "@/hooks/useFavoriteStore";
+import Card from "@/components/Card";
+
+const Favourite = () => {
+  const favorites = useFavoriteStore((state) => state.favorites);
+
+  return (
+    <View style={styles.container}>
+      <Text style={{ ...styles.heading, textAlign: "center" }}>Favorites</Text>
+
+      {favorites.length == 0 ? (
+        <View style={{ ...styles.container, height: "100%", width: "100%", display: "flex", justifyContent: "center", alignItems: "center" }}>
+          <Text style={{ ...styles.heading }}>There is no favorite movie yet!</Text>
+          <Text style={{ ...styles.text, opacity: 0.5 }}>Add your favorite movie first</Text>
+        </View>
+      ) : (
+        <FlatList
+          style={{ height: "100%" }}
+          numColumns={3}
+          data={favorites.filter((item) => item.poster_path)}
+          keyExtractor={(item) => item.id.toString()}
+          renderItem={({ item }) => <Card key={item.id} title={item.title} poster_path={item.poster_path} id={item.id} />}
+        />
+      )}
+    </View>
+  );
+};
+
+export const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#242A32",
+    padding: 12,
+  },
+  text: {
+    color: "white",
+  },
+
+  heading: {
+    color: "white",
+    fontSize: 20,
+    fontWeight: "500",
+  },
+});
+
+export default Favourite;
