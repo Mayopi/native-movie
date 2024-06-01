4f123f93cce1b4163636783b28c859beaa7363b6
Mayopi <cyade007@gmail.com>
2024-06-01 17:13:47 +0700

    feat: stack screen

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -2,8 +2,20 @@ import { Stack } from "expo-router";
 
 export default function RootLayout() {
   return (
-    <Stack>
+    <Stack
+      screenOptions={{
+        headerStyle: {
+          backgroundColor: "#242A32",
+        },
+        headerTintColor: "#fff",
+        headerTitleStyle: {
+          fontWeight: "bold",
+        },
+      }}
+    >
       <Stack.Screen name="index" />
+      <Stack.Screen name="details/[id]" />
+      <Stack.Screen name="favorite" />
     </Stack>
   );
 }
